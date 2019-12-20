b6c64e4916cdf520f1afefc7b067ac09e77bc28d
L6Y1 <2571874208@qq.com>
2019-12-20 22:17:22 +0800

    ly

---
+++ app/src/main/java/com/wzh/calendar/dao/DataCallBack.java
@@ -0,0 +1,10 @@
+package com.wzh.calendar.dao;
+
+/**
+ * Created by ly on 2019/12/19.
+ */
+
+public interface DataCallBack {
+    public void getDataForStart(String data);
+    public void getDataForStop(String data);
+}
